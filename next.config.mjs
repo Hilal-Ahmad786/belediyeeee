2da1dc0a1b0cf7d9ffa915733d63bdccf1e43b64
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-12 15:27:22 +0300

    Update Next.js configuration and links for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-// next.config.js
-module.exports = {
-  output: 'export',
-  basePath: '/belediyeeee',
-  assetPrefix: '/belediyeeee/',
-  images: {
-    unoptimized: true,
-  },
-};
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/belediyeeee/' : '',
+    basePath: '/belediyeeee',
+    images: {
+      unoptimized: true, // Necessary if using next/image with static export
+    },
+    trailingSlash: true,
+    output: 'export', // Add this line
+  };
+  
+  export default nextConfig;
+  
