850e243735c4e68870c6c6f34e3e7bb0bf132fd2
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-12 10:51:49 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/belediii/' : '',
+    basePath: '/belediii',
+    trailingSlash: true,
+    output: 'export', // Add this line
+  };
+  
+  export default nextConfig;
+  
