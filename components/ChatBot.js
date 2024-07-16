82bf7bb4da6a12fe7ce76bde3b2f8ef7448b7a41
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-16 20:05:33 +0300

    fljsdlfjslj

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -1,4 +1,3 @@
-// components/ChatBot.js
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import styles from '../app/ChatBot.module.css';
@@ -17,17 +16,17 @@ const ChatBot = () => {
   });
   const messagesEndRef = useRef(null);
 
-  useEffect(() => {
-    // İlk hoş geldin mesajını gönder
-    const welcomeMessage = { sender: 'bot', text: '🌟 Merhaba! Ben Bursa Smart Assistant. Size Bursa hakkında bilgi vermek için buradayım. Nasıl yardımcı olabilirim?' };
-    setMessages([welcomeMessage]);
-  }, []);
-
   const handleSend = async () => {
-    if (!input.trim() && step <= 12) return;
+    if (!input.trim()) return;
 
     const userMessage = { sender: 'user', text: input };
-    setMessages([...messages, userMessage]);
+    setMessages(prevMessages => [...prevMessages, userMessage]);
+
+    const lowerInput = input.toLowerCase();
+    if (step === 1 && (lowerInput.includes('şikayet') || lowerInput.includes('talep') || lowerInput.includes('istek'))) {
+      setStep(2);
+      setFormData(prevFormData => ({ ...prevFormData, applicationType: lowerInput }));
+    }
 
     let botMessageText = '';
 
@@ -37,72 +36,69 @@ const ChatBot = () => {
         setStep(2);
         break;
       case 2:
-        setFormData({ ...formData, applicationType: input });
-        botMessageText = 'Başvuru Alt Türünüzü Seçiniz:';
-        setStep(3);
+        botMessageText = 'Seçiniz:';
         break;
       case 3:
-        setFormData({ ...formData, applicationSubType: input });
+        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: input }));
         botMessageText = 'Başvuru İçin İlgili Birimi Seçiniz:';
         setStep(4);
         break;
       case 4:
-        setFormData({ ...formData, relevantUnit: input });
+        setFormData(prevFormData => ({ ...prevFormData, relevantUnit: input }));
         botMessageText = 'İsim:';
         setStep(5);
         break;
       case 5:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, name: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, name: input } }));
         botMessageText = 'Soyisim:';
         setStep(6);
         break;
       case 6:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, surname: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, surname: input } }));
         botMessageText = 'TC:';
         setStep(7);
         break;
       case 7:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, tc: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, tc: input } }));
         botMessageText = 'Cinsiyet:';
         setStep(8);
         break;
       case 8:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, gender: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, gender: input } }));
         botMessageText = 'Cep:';
         setStep(9);
         break;
       case 9:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, mobile: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, mobile: input } }));
         botMessageText = 'Eposta:';
         setStep(10);
         break;
       case 10:
-        setFormData({ ...formData, personalInfo: { ...formData.personalInfo, email: input } });
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, email: input } }));
         botMessageText = 'Konumu gir:';
         setStep(11);
         break;
       case 11:
-        setFormData({ ...formData, addressInfo: input });
+        setFormData(prevFormData => ({ ...prevFormData, addressInfo: input }));
         botMessageText = 'Adres gir:';
         setStep(12);
         break;
       case 12:
-        setFormData({ ...formData, applicationInfo: input });
+        setFormData(prevFormData => ({ ...prevFormData, applicationInfo: input }));
         botMessageText = 'Açıklama:';
         setStep(13);
         break;
       case 13:
-        // Summarize and send the application data
         await submitApplication(formData);
-        botMessageText = `Başvurunuz alındı, Teşekkürler.\n\nÖzet:\nBaşvuru Türü: ${formData.applicationType}\nBaşvuru Alt Türü: ${formData.applicationSubType}\nİlgili Birim: ${formData.relevantUnit}\nİsim: ${formData.personalInfo.name}\nSoyisim: ${formData.personalInfo.surname}\nTC: ${formData.personalInfo.tc}\nCinsiyet: ${formData.personalInfo.gender}\nCep: ${formData.personalInfo.mobile}\nEposta: ${formData.personalInfo.email}\nKonum: ${formData.addressInfo}\nAdres: ${formData.addressInfo}\nAçıklama: ${formData.applicationInfo}`;
+        botMessageText = `Başvurunuz alındı, teşekkürler.\n\nÖzet:\nBaşvuru Türü: ${formData.applicationType}\nBaşvuru Alt Türü: ${formData.applicationSubType}\nİlgili Birim: ${formData.relevantUnit}\nİsim: ${formData.personalInfo.name}\nSoyisim: ${formData.personalInfo.surname}\nTC: ${formData.personalInfo.tc}\nCinsiyet: ${formData.personalInfo.gender}\nCep: ${formData.personalInfo.mobile}\nEposta: ${formData.personalInfo.email}\nKonum: ${formData.addressInfo}\nAdres: ${formData.addressInfo}\nAçıklama: ${formData.applicationInfo}`;
         setStep(14);
         break;
       default:
         botMessageText = 'Başvurunuz tamamlandı.';
     }
 
-    const botMessage = { sender: 'bot', text: `🌟 ${botMessageText}` };
-    setMessages([...messages, userMessage, botMessage]);
+    const botMessage = { sender: 'bot', text: botMessageText };
+    setMessages(prevMessages => [...prevMessages, userMessage, botMessage]);
     setInput('');
   };
 
@@ -114,11 +110,17 @@ const ChatBot = () => {
       console.log(response.data);
     } catch (error) {
       console.error('Error submitting application:', error);
+      const botMessage = { sender: 'bot', text: 'Başvurunuzu gönderirken bir hata oluştu. Lütfen tekrar deneyin.' };
+      setMessages(prevMessages => [...prevMessages, botMessage]);
     }
   };
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+    if (messages.length === 0) {
+      setMessages([{ sender: 'bot', text: 'Merhaba! Ben Bursa Smart Assistant. Size Bursa hakkında bilgi vermek için buradayım. Nasıl yardımcı olabilirim?' }]);
+    }
   }, [messages]);
 
   const renderDropdown = (options, onChange, heading) => (
@@ -144,46 +146,42 @@ const ChatBot = () => {
             <p><strong>{msg.sender === 'user' ? 'You' : 'ChatBot'}:</strong> {msg.text}</p>
           </div>
         ))}
-        <div ref={messagesEndRef} />  {/* This div helps scroll to the latest message */}
+        <div ref={messagesEndRef} />
       </div>
       {step === 1 && renderDropdown(['Talep', 'Şikayet', 'Öneri'], value => {
-        setFormData({ ...formData, applicationType: value });
-        setMessages([...messages, { sender: 'user', text: value }]);
+        setFormData(prevFormData => ({ ...prevFormData, applicationType: value }));
+        setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
         setStep(2);
       }, 'Başvuru Türünüzü Seçiniz')}
       {step === 2 && formData.applicationType === 'Talep' && renderDropdown(['Sosyal Yardım Talebi', 'Bilgi Edinme', 'Ruhsat', 'İmar'], value => {
-        setFormData({ ...formData, applicationSubType: value });
-        setMessages([...messages, { sender: 'user', text: value }]);
+        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
+        setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
         setStep(3);
       }, 'Seçiniz')}
       {step === 2 && formData.applicationType === 'Şikayet' && renderDropdown(['Ulaşım', 'Temizlik', 'Zabıta', 'İmar', 'Aydınlatma'], value => {
-        setFormData({ ...formData, applicationSubType: value });
-        setMessages([...messages, { sender: 'user', text: value }]);
+        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
+        setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
         setStep(3);
       }, 'Seçiniz')}
       {step === 2 && formData.applicationType === 'Öneri' && renderDropdown(['Ulaşım', 'Temizlik', 'Kültür', 'Eğitim', 'Teknoloji'], value => {
-        setFormData({ ...formData, applicationSubType: value });
-        setMessages([...messages, { sender: 'user', text: value }]);
+        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
+        setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
         setStep(3);
       }, 'Seçiniz')}
-      {step === 3 && renderDropdown(['Genel Sekreterlik', 'Bilgi İşlem', 'İnsan Kaynakları', 'Park Bahçeler', 'Ulaşım ', 'Diğer  '], value => {
-        setFormData({ ...formData, relevantUnit: value });
-        setMessages([...messages, { sender: 'user', text: value }]);
-        setStep(4);
-      }, 'Başvuru İçin İlgili Birimi Seçiniz')}
-      {step >= 4 && step <= 13 && (
-        <div className={styles['input-container']}>
+      {step >= 3 && step <= 13 && (
+        <div className={styles['chatbot-input']}>
           <input
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className={styles['text-input']}
+            onKeyDown={(e) => e.key === 'Enter' && handleSend()}
+            placeholder="Mesajınızı yazın..."
           />
-          <button onClick={handleSend} className={styles['send-button']}>Gönder</button>
+          <button onClick={handleSend}>Gönder</button>
         </div>
       )}
     </div>
   );
 };
 
-export default ChatBot;
+export default ChatBot;
