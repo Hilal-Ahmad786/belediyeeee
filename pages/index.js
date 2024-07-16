f00156f45ff30e08b83eaaf28f71f0a14f823bb5
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-16 15:06:02 +0300

    lfdlsfjldjslfjls

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,73 @@
 // pages/index.js
 import styled from '@emotion/styled';
 import Link from 'next/link';
-import getConfig from 'next/config';
-import styles from '../app/Home.module.css';
-import Chatbot from '../components/ChatBot';
-
-
-const { publicRuntimeConfig } = getConfig();
 
 const Container = styled.div`
   display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  height: 100vh;
+  padding: 20px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100vh;
+  margin: 20px;
 `;
 
 const Button = styled.button`
-  padding: 16px 32px;
+  width: 200px;
+  padding: 16px 0;
   color: white;
+  background-color: #28a745;
   border: none;
   border-radius: 8px;
   cursor: pointer;
   font-size: 16px;
+  margin: 10px 0;
+  text-align: center;
+`;
+
+const VideoContainer = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 600px;
+  height: 400px;
 `;
 
 const Home = () => {
   return (
-    <div className={styles.homeBackground}>
-    <Container className="text-center" style={{ position: 'relative', zIndex: 1 }}>
-    <h1 className="display-4 text-light">Welcome to Modern Bursa</h1>
-    <p className="lead text-light">Discover the vibrant and contemporary side of Bursa while appreciating its rich history.</p>
-      <Link href={`${publicRuntimeConfig.basePath}/step1`}>
-        <Button className='btn btn-success'>Start Form</Button>
-      </Link>
-      <Chatbot />
+    <Container>
+      <ButtonContainer>
+        <Link href="/step4">
+          <Button>Kişisel bilgiler</Button>
+        </Link>
+        <Link href="https://www.cimer.gov.tr/">
+          <Button>Cimer</Button>
+        </Link>
+        <Link href="https://www.turkiye.gov.tr/icisleri-sosyal-yardim-talebinde-bulunma-ve-talebi-sorgulama-4671">
+          <Button>Sosyal Yardim</Button>
+        </Link>
+      </ButtonContainer>
 
+      <ButtonContainer>
+        <Link href="https://www.osmangazi.bel.tr/tr/basvuru-rehberi/diger-islemler">
+          <Button>Basvuru Rehberi</Button>
+        </Link>
+        <Link href="https://osmangazi.bel.tr/anket/">
+          <Button>Anket</Button>
+        </Link>
+      </ButtonContainer>
+
+      <VideoContainer>
+        <video width="100%" height="100%" controls>
+          <source src="/video.mp4" type="video1/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </VideoContainer>
     </Container>
-        </div>
-
   );
 };
 
