fc900659328e230bc65ceb856fba2892822df70c
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-17 12:52:21 +0300

    kfksdfkhkjdhk

--- components/Chatbox.js
+++ components/Chatbox.js
@@ -28,7 +28,7 @@ const ChatBot = () => {
     setMessages(prevMessages => [...prevMessages, userMessage]);
 
     const lowerInput = input.toLowerCase();
-    if (step === 1 && (lowerInput.includes('şikayet') || lowerInput.includes('talep') || lowerInput.includes('istek'))) {
+    if (step === 1 && (lowerInput.includes('şikayet') || lowerInput.includes('talep') || lowerInput.includes('öneri'))) {
       setStep(2);
       setFormData(prevFormData => ({ ...prevFormData, applicationType: lowerInput }));
     }
@@ -37,65 +37,64 @@ const ChatBot = () => {
 
     switch (step) {
       case 1:
-        botMessageText = 'Başvuru Türünüzü Seçiniz:';
-        setStep(2);
+        botMessageText = 'Size nasıl yardımcı olabilirim?';
         break;
       case 2:
-        botMessageText = 'Seçiniz:';
+        botMessageText = 'Başvuru Türünüzü Seçiniz:';
+        setStep(3);
         break;
       case 3:
-        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: input }));
-        botMessageText = 'Başvuru İçin İlgili Birimi Seçiniz:';
-        setStep(4);
+        botMessageText = 'Seçiniz:';
         break;
       case 4:
-        setFormData(prevFormData => ({ ...prevFormData, relevantUnit: input }));
-        botMessageText = 'İsim:';
+        setFormData(prevFormData => ({ ...prevFormData, applicationSubType: input }));
+        botMessageText = 'Başvuru İçin İlgili Birimi Seçiniz:';
         setStep(5);
         break;
       case 5:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, name: input } }));
-        botMessageText = 'Soyisim:';
+        setFormData(prevFormData => ({ ...prevFormData, relevantUnit: input }));
+        botMessageText = 'İsim:';
         setStep(6);
         break;
       case 6:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, surname: input } }));
-        botMessageText = 'TC:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, name: input } }));
+        botMessageText = 'Soyisim:';
         setStep(7);
         break;
       case 7:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, tc: input } }));
-        botMessageText = 'Cinsiyet:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, surname: input } }));
+        botMessageText = 'TC:';
         setStep(8);
         break;
       case 8:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, gender: input } }));
-        botMessageText = 'Cep:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, tc: input } }));
+        botMessageText = 'Cinsiyet:';
         setStep(9);
         break;
       case 9:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, mobile: input } }));
-        botMessageText = 'Eposta:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, gender: input } }));
+        botMessageText = 'Cep:';
         setStep(10);
         break;
       case 10:
-        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, email: input } }));
-        botMessageText = 'Konumu gir:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, mobile: input } }));
+        botMessageText = 'Eposta:';
         setStep(11);
         break;
       case 11:
-        setFormData(prevFormData => ({ ...prevFormData, addressInfo: input }));
-        botMessageText = 'Adres gir:';
+        setFormData(prevFormData => ({ ...prevFormData, personalInfo: { ...prevFormData.personalInfo, email: input } }));
+        botMessageText = 'Konumu gir:';
         setStep(12);
         break;
       case 12:
-        setFormData(prevFormData => ({ ...prevFormData, applicationInfo: input }));
-        botMessageText = 'Açıklama:';
+        setFormData(prevFormData => ({ ...prevFormData, addressInfo: input }));
+        botMessageText = 'Adres gir:';
         setStep(13);
         break;
       case 13:
-        await submitApplication(formData);
+        setFormData(prevFormData => ({ ...prevFormData, applicationInfo: input }));
         botMessageText = `Başvurunuz alındı, teşekkürler.\n\nÖzet:\nBaşvuru Türü: ${formData.applicationType}\nBaşvuru Alt Türü: ${formData.applicationSubType}\nİlgili Birim: ${formData.relevantUnit}\nİsim: ${formData.personalInfo.name}\nSoyisim: ${formData.personalInfo.surname}\nTC: ${formData.personalInfo.tc}\nCinsiyet: ${formData.personalInfo.gender}\nCep: ${formData.personalInfo.mobile}\nEposta: ${formData.personalInfo.email}\nKonum: ${formData.addressInfo}\nAdres: ${formData.addressInfo}\nAçıklama: ${formData.applicationInfo}`;
+        await submitApplication(formData);
         setStep(14);
         break;
       default:
@@ -123,18 +122,6 @@ const ChatBot = () => {
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
-
-  const renderDropdown = (options, onChange, heading) => (
-    <div>
-      <h4>{heading}</h4>
-      <select className={styles.dropdown} onChange={(e) => onChange(e.target.value)}>
-        <option value="">Seçiniz</option>
-        {options.map(option => (
-          <option key={option} value={option}>{option}</option>
-        ))}
-      </select>
-    </div>
-  );
 
   return (
     <div style={{ position: 'fixed', bottom: 0, right: 0, margin: 20, zIndex: 1000 }}>
@@ -196,42 +183,20 @@ const ChatBot = () => {
             ))}
             <div ref={messagesEndRef} />
           </div>
-          {step === 1 && renderDropdown(['Talep', 'Şikayet', 'Öneri'], value => {
-            setFormData(prevFormData => ({ ...prevFormData, applicationType: value }));
-            setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
-            setStep(2);
-          }, 'Başvuru Türünüzü Seçiniz')}
-          {step === 2 && formData.applicationType === 'Talep' && renderDropdown(['Sosyal Yardım Talebi', 'Bilgi Edinme', 'Ruhsat', 'İmar'], value => {
-            setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
-            setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
-            setStep(3);
-          }, 'Seçiniz')}
-          {step === 2 && formData.applicationType === 'Şikayet' && renderDropdown(['Ulaşım', 'Temizlik', 'Zabıta', 'İmar', 'Aydınlatma'], value => {
-            setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
-            setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
-            setStep(3);
-          }, 'Seçiniz')}
-          {step === 2 && formData.applicationType === 'Öneri' && renderDropdown(['Ulaşım', 'Temizlik', 'Kültür', 'Eğitim', 'Teknoloji'], value => {
-            setFormData(prevFormData => ({ ...prevFormData, applicationSubType: value }));
-            setMessages(prevMessages => [...prevMessages, { sender: 'user', text: value }]);
-            setStep(3);
-          }, 'Seçiniz')}
-          {step >= 3 && step <= 13 && (
-            <div className={styles['chatbot-input']}>
-              <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                onKeyDown={(e) => e.key === 'Enter' && handleSend()}
-                placeholder="Mesajınızı yazın..."
-              />
-              <button onClick={handleSend}>Gönder</button>
-            </div>
-          )}
+          <div className={styles['chatbot-input']}>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === 'Enter' && handleSend()}
+              placeholder="Mesajınızı yazın..."
+            />
+            <button onClick={handleSend}>Gönder</button>
+          </div>
         </div>
       )}
     </div>
   );
 };
 
-export default ChatBot;
+export default ChatBot;
