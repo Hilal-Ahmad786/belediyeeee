881ac4c3f9688dcebabab5b58118145e9204b345
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2024-07-17 12:17:43 +0300

    jldfjslfjl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-// next.config.mjs
 const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
 const repoName = 'belediyeeee'; // Replace with your repository name
 
@@ -9,6 +8,8 @@ export default {
   images: {
     unoptimized: true, // Necessary for GitHub Pages
   },
+  output: 'export', // Ensures next export is used
+  trailingSlash: true, // Ensures paths end with a trailing slash
   publicRuntimeConfig: {
     basePath: isGitHubPages ? `/${repoName}` : '',
   },
